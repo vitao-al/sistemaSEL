0cea6cb484929a4aeb79ba6addb1df332f009155
vitao-al <vm74531@gmail.com>
2026-09-26 15:11:16 -0300

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: 'cover',
+  themeColor: '#9A0020',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
